79092736d5556b14a1bec5f42fd023da72c6add1
Mimerme <androssdragonq.9@gmail.com>
2015-07-21 15:16:09 -0400

    Update release

--- Yak-Hax-Mimerme/release/release.bat
+++ Yak-Hax-Mimerme/release/release.bat
@@ -1,3 +1,3 @@
 @ECHO off
-if not exists values.txt echo. 2>values.txt
+if not exists %UTILS_PATH%\yhack\values.txt echo. 2>values.txt
 java -jar %UTILS_PATH%\yhack\yhack.jar %1 %2 %3 %4 %5 %6 %7 %8
