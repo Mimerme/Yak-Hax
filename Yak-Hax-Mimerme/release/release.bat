31ac693c8df343b43e948fe9a3bf233c991f1676
Mimerme <androssdragonq.9@gmail.com>
2015-07-21 15:18:44 -0400

    Update batch

--- Yak-Hax-Mimerme/release/release.bat
+++ Yak-Hax-Mimerme/release/release.bat
@@ -1,3 +1,3 @@
 @ECHO off
-if not exists %UTILS_PATH%\yhack\values.txt echo. 2>values.txt
+if not exists %UTILS_PATH%\yhack\values.txt echo "File not exists"
 java -jar %UTILS_PATH%\yhack\yhack.jar %1 %2 %3 %4 %5 %6 %7 %8
